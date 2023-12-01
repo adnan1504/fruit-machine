edf02feb8882b06a9672b54c7a61db0b62b3c01f
Adnan Shamim <231204@student.wqe.ac.uk>
2023-12-01 13:20:40 +0000

    fruits machine

--- fruit machine/Form1.Designer.cs
+++ fruit machine/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -85,30 +86,40 @@
             // 
             // timer1
             // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 150;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // pictureBox4
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(171, 75);
+            this.pictureBox4.Location = new System.Drawing.Point(171, 104);
             this.pictureBox4.Name = "pictureBox4";
             this.pictureBox4.Size = new System.Drawing.Size(100, 50);
             this.pictureBox4.TabIndex = 5;
             this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
             this.pictureBox4.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox4_Paint);
             // 
             // pictureBox5
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(171, 268);
+            this.pictureBox5.Location = new System.Drawing.Point(171, 242);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(100, 50);
             this.pictureBox5.TabIndex = 6;
             this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            this.pictureBox5.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox5_Paint);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(812, 463);
+            this.ClientSize = new System.Drawing.Size(796, 469);
             this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.button1);
@@ -135,6 +146,7 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Timer timer2;
     }
 }
 
--- fruit machine/Form1.cs
+++ fruit machine/Form1.cs
@@ -20,7 +20,11 @@ namespace fruit_machine
         static Image lemon = fruit_machine.Resource1.fruit6;
         static Image berry = fruit_machine.Resource1.fruit7;
         Image[] images = { apple,pear,mango,orange,cherry,lemon,berry };
-    
+        int pointer = 0;
+        int pointer1 = -1;
+       
+
+
 
         public Form1()
         {
@@ -54,34 +58,67 @@ namespace fruit_machine
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-           for (int i = 0;i<images.Length;i++)
-            {
-                  
-                e.Graphics.DrawImage(images[i],0,0);
-                
-            }
+           
            
 
         }
 
         private void pictureBox2_Paint(object sender, PaintEventArgs e)
         {
-            e.Graphics.DrawImage(pear, 0, 0);
+            
         }
 
         private void pictureBox3_Paint(object sender, PaintEventArgs e)
         {
-            e.Graphics.DrawImage(mango , 0, 0);
+            
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-
+            Random rnd = new Random();
+            int rand_num = rnd.Next(0,6);
+            
+            pointer++;
+            if(pointer>=images.Length)
+            {
+                pointer= 0;
+            }
+            pictureBox1.Image = images[pointer];
+            pictureBox2.Image = images[pointer];
+            pictureBox3.Image = images[pointer];
+            
         }
 
         private void pictureBox4_Paint(object sender, PaintEventArgs e)
         {
+            
+        }
+
+        private void pictureBox5_Paint(object sender, PaintEventArgs e)
+        {
             e.Graphics.DrawImage(mango, 0, 0);
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int rand_num = rnd.Next(0, 6);
+            pointer1++;
+            if (pointer1 >= images.Length)
+            {
+                pointer1 = 0;
+            }
+            pictureBox4.Image = images[pointer];
         }
     }
 }
